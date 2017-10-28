3fc6356d42944c85a56418b04f59b69495e62aaf
Divyansh Mahajan <divyanshmahajan.24@gmail.com>
2017-10-28 21:22:18 +0530

    Add Tab component

--- src/components/Main/MainComponent.js
+++ src/components/Main/MainComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../Header';
+import Tabs from '../Tabs';
 
-const Main = () => [<Header key="0" />];
+const Main = () => [<Header key="0" />, <Tabs key="1" />];
 
 export default Main;
